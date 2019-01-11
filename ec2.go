2bbc8002523111c0ad02f16e9b63c1378d7bdd69
hogehoge789 <hogehoge789@gmail.com>
2019-01-11 20:10:19 +0900

    create vpc

--- ec2.go
+++ ec2.go
@@ -10,6 +10,14 @@ import (
 
 func main() {
 	sess := session.Must(session.NewSession())
-	svc := ec2.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
-	fmt.Println(svc.DescribeAccountAttributes(nil))
+	ec2svc := ec2.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
+
+	ec2svc.CreateVpc(&ec2.CreateVpcInput{
+		CidrBlock:                   aws.String("10.0.0.0/16"),
+		AmazonProvidedIpv6CidrBlock: aws.Bool(false),
+		InstanceTenancy:             aws.String("default"),
+	})
+
+	fmt.Println(ec2svc.DescribeAccountAttributes(nil))
+	fmt.Println(ec2svc.DescribeNatGateways(nil))
 }
